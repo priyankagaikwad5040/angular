e6ef9a7dbbaa8be219d3d97a2eb6e77269df525a
priyankagaikwad5040 <priyankagaikwad5040@gmail.com>
2024-05-20 17:26:19 +0530

    component creation
    component creation

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Demo } from './demo/demo.component';
 import { Demo2 } from './demo2/demo2.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Demo,
-    Demo2
+    Demo2,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
 
   
   ],
--- angularapp/src/app/demo2/demo2.component.ts
+++ angularapp/src/app/demo2/demo2.component.ts
@@ -3,8 +3,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector :'app-demo2',
-    template :'<h1>Priyanka</h1><h1>Gaikwad</h1>'
-})
+    template :`<h1> Demo Component </h1>
+    <button class="btn btn-primary">Save</button>`})
+
 export class Demo2{
 
 }
